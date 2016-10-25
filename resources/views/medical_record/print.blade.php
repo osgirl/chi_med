fa8f9b13d9a37393f37ddc735f75ec6ed783f62b
garrychiao <g50905g@gmail.com>
2016-10-25 15:53:04 +0800

    font-family-set

--- resources/views/medical_record/print.blade.php
+++ resources/views/medical_record/print.blade.php
@@ -18,189 +18,189 @@
           <table border="1"  width="100%">
             <tr class="warning">
               <th width="125px">
-                <p>
+
                   {{ $record->surname}} {{$record->last_name}}
-                </p>
+                
               </th>
               <th>
-                <p>
+
                   DOB : {{ date('d-m-Y', strtotime($record->DOB))}}
-                </p>
+                
               </th>
               <th>
-                <p>
+
                   No.
-                </p>
+                
               </th>
               <td width="125px">
-                <p>
+
                   {{ $record->treatment_number }}
-                </p>
+                
               </td>
               <td>
-                <p>
+
                   Date
-                </p>
+                
               </td>
               <td>
-                <p>
+
                   {{ date('d-m-Y', strtotime($record->date))}}
-                </p>
+                
               </td>
             </tr>
             <tr>
               <th class="info" colspan="6">
-                <p>
+
                   Symptoms
-                </p>
+                
               </th>
             </tr>
             <tr>
               <td class="warning">
-                <p>
+
                   Main Complaint / S
-                </p>
+                
               </td>
               <td colspan="5">
-                <p>
+
                   {{ $record->main_complaint}}
-                </p>
+                
               </td>
             </tr>
             <tr>
               <td class="warning">
-                <p>
+
                   Current Condition and Accompanied Symptoms
-                </p>
+                
               </td>
               <td colspan="5">
-                <p>
+
                   {{ $record->symptoms}}
-                </p>
+                
               </td>
             </tr>
             <tr>
               <td class="warning">
-                <p>
+
                   General Question
-                </p>
+                
               </td>
               <td colspan="5">
-                <p>
+
                   {{ $record->general_question }}
-                </p>
+                
               </td>
             </tr>
             <tr>
               <td class="warning">
-                <p>
+
                   Current Physical Examinations
-                </p>
+                
               </td>
               <td colspan="5">
-                <p>
+
                   {{ $record->physical_examinations}}
-                </p>
+                
               </td>
             </tr>
             <tr class="info">
               <th colspan="6">
-                <p>
+
                   Tongue:
-                </p>
+                
               </th>
             </tr>
             <tr>
               <td class="col-sm-1 warning">
-                <p>
+
                   Tongue Texture
-                </p>
+                
               </td>
               <td colspan="2" class="col-sm-3">
-                <p>
+
                   {{ $record->tongue_status }}
-                </p>
+                
               </td>
               <td class="col-sm-1 warning">
-                <p>
+
                   Body Colour
-                </p>
+                
               </td>
               <td colspan="2" class="col-sm-3">
-                <p>
+
                   {{ $record->body_colour}}
-                </p>
+                
               </td>
             </tr>
             <tr>
-              <td class="warning"><p>Shape</p></td>
-              <td colspan="2"><p>{{ $record->shape}}</p></td>
-              <td class="warning"><p>Movement</p></td>
-              <td colspan="2"><p>{{$record->movement}}</p></td>
+              <td class="warning">Shape</td>
+              <td colspan="2">{{ $record->shape}}</td>
+              <td class="warning">Movement</td>
+              <td colspan="2">{{$record->movement}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Proper of Coating</p></td>
-              <td colspan="2"><p>{{ $record->proper_of_coating}}</p></td>
-              <td class="warning"><p>Coating Colour</p></td>
-              <td colspan="2"><p>{{ $record->coating_colour}}</p></td>
+              <td class="warning">Proper of Coating</td>
+              <td colspan="2">{{ $record->proper_of_coating}}</td>
+              <td class="warning">Coating Colour</td>
+              <td colspan="2">{{ $record->coating_colour}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Pulses (Overall Speed)</p></td>
+              <td class="warning">Pulses (Overall Speed)</td>
               <td colspan="5">
-                <p>
+
                   {{ $record->pulses }}
-                </p>
+                
               </td>
             </tr>
             <tr class="warning">
-              <td colspan="3" align="center"><p>Right</p></td>
-              <td colspan="3" align="center"><p>Left</p></td>
+              <td colspan="3" align="center">Right</td>
+              <td colspan="3" align="center">Left</td>
             </tr>
             <tr>
-              <td class="warning"><p>Lung (qi)</p></td>
-              <td colspan="2"><p>{{ $record->lung_qi}}</p></td>
-              <td class="warning"><p>Heart (blood)</p></td>
-              <td colspan="2"><p>{{ $record->heart_blood}}</p></td>
+              <td class="warning">Lung (qi)</td>
+              <td colspan="2">{{ $record->lung_qi}}</td>
+              <td class="warning">Heart (blood)</td>
+              <td colspan="2">{{ $record->heart_blood}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Spleen</p></td>
-              <td colspan="2"><p>{{ $record->spleen}}</p></td>
-              <td class="warning"><p>Liver</p></td>
-              <td colspan="2"><p>{{ $record->liver}}</p></td>
+              <td class="warning">Spleen</td>
+              <td colspan="2">{{ $record->spleen}}</td>
+              <td class="warning">Liver</td>
+              <td colspan="2">{{ $record->liver}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Kidney (yang / qi)</p></td>
-              <td colspan="2"><p>{{ $record->kidney_yang}}</p></td>
-              <td class="warning"><p>Kidney (yin)</p></td>
-              <td colspan="2"><p>{{ $record->kidney_yin}}</p></td>
+              <td class="warning">Kidney (yang / qi)</td>
+              <td colspan="2">{{ $record->kidney_yang}}</td>
+              <td class="warning">Kidney (yin)</td>
+              <td colspan="2">{{ $record->kidney_yin}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>TCM Disease</p></td>
-              <td colspan="5"><p>{{ $record->TCM_disease}}</p></td>
+              <td class="warning">TCM Disease</td>
+              <td colspan="5">{{ $record->TCM_disease}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>TCM Type / Pattern</p></td>
-              <td colspan="5"><p>{{ $record->TCM_type}}</p></td>
+              <td class="warning">TCM Type / Pattern</td>
+              <td colspan="5">{{ $record->TCM_type}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Treatment Principle</p></td>
-              <td colspan="5"><p>{{ $record->treatment_principle}}</p></td>
+              <td class="warning">Treatment Principle</td>
+              <td colspan="5">{{ $record->treatment_principle}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Acu-points & Techniques/s & Methods/s</p></td>
-              <td colspan="5"><p>{{ $record->Acu_points}}</p></td>
+              <td class="warning">Acu-points & Techniques/s & Methods/s</td>
+              <td colspan="5">{{ $record->Acu_points}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Explanation Of Treatment</p></td>
-              <td colspan="5"><p>{{ $record->treatment_explanation}}</p></td>
+              <td class="warning">Explanation Of Treatment</td>
+              <td colspan="5">{{ $record->treatment_explanation}}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Cautions, Contraindications and Red Flag</p></td>
-              <td colspan="5"><p>{{ $record->cautions }}</p></td>
+              <td class="warning">Cautions, Contraindications and Red Flag</td>
+              <td colspan="5">{{ $record->cautions }}</td>
             </tr>
             <tr>
-              <td class="warning"><p>Post Treatment Advice</p></td>
-              <td colspan="5"><p>{{ $record->treatment_adjustments}}</p></td>
+              <td class="warning">Post Treatment Advice</td>
+              <td colspan="5">{{ $record->treatment_adjustments}}</td>
             </tr>
             <tr style="height:75px;">
               <td class="warning">
