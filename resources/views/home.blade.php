ae6c748d8d817ad1fc3d18034d90d284f900250b
garrychiao <g50905g@gmail.com>
2016-11-21 04:50:39 +0800

    pe pics added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,4 +44,17 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+  var d = new Date();
+  var weekday = new Array(7);
+  weekday[0]=  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  document.getElementById('weekday_now').innerHTML = weekday[d.getDay()];
+  document.getElementById('date_now').innerHTML = d.getDate()+"-"+d.getMonth(d.setMonth(d.getMonth()+1))+"-"+d.getFullYear();
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,18 +195,5 @@
     <script type="text/javascript">
       $('select').select2();
     </script>
-    <script type="text/javascript">
-      var d = new Date();
-      var weekday = new Array(7);
-      weekday[0]=  "Sunday";
-      weekday[1] = "Monday";
-      weekday[2] = "Tuesday";
-      weekday[3] = "Wednesday";
-      weekday[4] = "Thursday";
-      weekday[5] = "Friday";
-      weekday[6] = "Saturday";
-      document.getElementById('weekday_now').innerHTML = weekday[d.getDay()];
-      document.getElementById('date_now').innerHTML = d.getDate()+"-"+d.getMonth(d.setMonth(d.getMonth()+1))+"-"+d.getFullYear();
-    </script>
 </body>
 </html>
